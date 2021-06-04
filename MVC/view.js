03498c99cbd6ed777ae02a711aae56cbb542ca18
MortenWaage <84008835+MortenWaage@users.noreply.github.com>
2021-06-04 19:01:28 +0200

    Update view.js

--- MVC/view.js
+++ MVC/view.js
@@ -1,7 +1,7 @@
 function updateView()
 {
     document.getElementById("infoBoard").innerHTML =
-    `   <div>Informasjon:</div><p></p>
+    `   <div>Informasjon:</div>
         <div id="Avfyrte skudd">Du har avfyrt ${shotsFired} skudd</div>
         <div id="Skutt Ned skudd">Du har skutt ned ${invadersKilled} invader${aboveOne}</div>
         <div id="ShotsMissed">${shotString}</div>`
